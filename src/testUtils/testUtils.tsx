0095d1ca0cd1a92d22fd1ea55c3917558d1e2ca8
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-29 21:24:31 +0200

    Add wrapWithRouter test utils function

--- src/testUtils/testUtils.tsx
+++ src/testUtils/testUtils.tsx
@@ -1,8 +1,11 @@
 import { PreloadedState } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import { RootState, setupStore, store } from "../store";
 import { PropsWithChildren } from "react";
 import { render } from "@testing-library/react";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { RootState, setupStore, store } from "../store";
+import theme from "../styles/theme/theme";
 
 export const renderWithProviders = (
   ui: React.ReactElement,
@@ -10,9 +13,26 @@ export const renderWithProviders = (
 ) => {
   const testStore = preloadedState ? setupStore(preloadedState) : store;
 
-  const Wrapper = ({ children }: PropsWithChildren): JSX.Element => {
-    return <Provider store={testStore}>{children}</Provider>;
+  const Wrapper = ({ children }: PropsWithChildren): React.ReactElement => {
+    return (
+      <Provider store={testStore}>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </Provider>
+    );
   };
 
   render(ui, { wrapper: Wrapper });
+};
+
+export const wrapWithRouter = (ui: React.ReactElement) => {
+  const routes = [
+    {
+      path: "/",
+      element: ui,
+    },
+  ];
+
+  const router = createMemoryRouter(routes);
+
+  return <RouterProvider router={router} />;
 };
