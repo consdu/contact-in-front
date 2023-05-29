cfde42a7d49f41385686ec5de5be3ad9013cba05
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-29 18:27:06 +0200

    Add / route to appRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
 import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { store } from "./store";
 import GlobalStyle from "./styles/GlobalStyle/GlobalStyle";
 import theme from "./styles/theme/theme";
+import appRouter from "./routers/appRouter/appRouter";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <App />
+        <RouterProvider router={appRouter} />
         <GlobalStyle />
       </ThemeProvider>
     </Provider>
