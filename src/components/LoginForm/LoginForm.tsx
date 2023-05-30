630ae72f36bacc2a52a98d5b981624601a874dc1
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-30 17:24:53 +0200

    Add onLoginFormSubmit functions as prop to Login Form component

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,22 +1,34 @@
 import { IoPersonCircleSharp } from "react-icons/io5";
 import LoginFormStyled from "./LoginFormStyled";
 import { useState } from "react";
+import { UserCredentials } from "../../types";
 
-const LoginForm = (): React.ReactElement => {
+interface LoginFormProps {
+  onLoginFormSubmit: (formData: UserCredentials) => void;
+}
+
+const LoginForm = ({
+  onLoginFormSubmit,
+}: LoginFormProps): React.ReactElement => {
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData((previousData) => ({
-      ...previousData,
+    setFormData((previousFormData) => ({
+      ...previousFormData,
       [event.target.id]: event.target.value,
     }));
   };
 
+  const handleLoginFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onLoginFormSubmit(formData);
+  };
+
   return (
-    <LoginFormStyled className="login-form">
+    <LoginFormStyled className="login-form" onSubmit={handleLoginFormSubmit}>
       <h2 className="login-form__title">Login to access your contacts</h2>
       <div className="login-form__group">
         <label htmlFor="username" hidden>
@@ -27,6 +39,7 @@ const LoginForm = (): React.ReactElement => {
             type="text"
             id="username"
             placeholder="Username"
+            autoComplete="off"
             value={formData.username}
             onChange={handleInputChange}
           />
@@ -43,15 +56,11 @@ const LoginForm = (): React.ReactElement => {
           type="password"
           id="password"
           placeholder="Password"
-          className="form-group__input"
+          value={formData.password}
           onChange={handleInputChange}
         />
       </div>
-      <button
-        className="login-form__button"
-        type="submit"
-        value={formData.password}
-      >
+      <button className="login-form__button" type="submit">
         Login
       </button>
     </LoginFormStyled>
