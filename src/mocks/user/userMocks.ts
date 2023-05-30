353bd6892c96a7585640bf0549f1e994c2427b6d
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-30 17:42:44 +0200

    Add tests for Login Form inputs typing use-case

--- src/mocks/user/userMocks.ts
+++ src/mocks/user/userMocks.ts
@@ -1,4 +1,10 @@
 import { UserStateStructure, UserTokenStructure } from "../../store/user/types";
+import { UserCredentials } from "../../types";
+
+export const userLoginDataMock: UserCredentials = {
+  username: "admin",
+  password: "admin",
+};
 
 export const userTokenMock =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDZmNmE5ZGExYjhhMTZiNDVlYWJmNDQiLCJuYW1lIjoiQWRtaW4iLCJpYXQiOjE2ODU0NDE5NzcsImV4cCI6MTY4NjA0Njc3N30.OKyDa1zjLR1_WNi-lZ9A7dL6DEAyK7KQ5EladkumYWE";
