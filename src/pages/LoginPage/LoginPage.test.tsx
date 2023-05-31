113609304c7a08baf50a6f3d2212cd02fff579da
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-05-31 16:37:35 +0200

    Add test for user logging in  with valid credentials

--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -1,8 +1,11 @@
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import LoginPage from "./LoginPage";
 import { renderWithProviders, wrapWithRouter } from "../../testUtils/testUtils";
+import { userLoginDataMock } from "../../mocks/user/userMocks";
 
-describe("Given a LoginPage", () => {
+describe("Given a LoginPage component", () => {
   describe("When rendered", () => {
     test("Then it should show the title and logo of the app", () => {
       const appName = "contact";
@@ -15,6 +18,43 @@ describe("Given a LoginPage", () => {
 
       expect(appTitle).toBeInTheDocument();
       expect(appLogo).toBeInTheDocument();
+    });
+  });
+
+  describe("When the rendered and the user logs in with invalid credentials", () => {
+    const usernameLabel = "Username:";
+    const passwordLabel = "Password:";
+    const buttonText = "Login";
+
+    test("Then it should redirect the user to the contacts page", async () => {
+      const expectedNewPath = "/contacts";
+
+      const routes = [
+        {
+          path: "/",
+          element: <LoginPage />,
+        },
+        {
+          path: "/contacts",
+          element: <p>Contacts page</p>,
+        },
+      ];
+
+      const router = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={router} />);
+
+      const usernameInput = screen.getByLabelText(usernameLabel);
+      const passwordInput = screen.getByLabelText(passwordLabel);
+      const loginbutton = screen.getByRole("button", {
+        name: buttonText,
+      });
+
+      await userEvent.type(usernameInput, userLoginDataMock.username);
+      await userEvent.type(passwordInput, userLoginDataMock.password);
+      await userEvent.click(loginbutton);
+
+      expect(router.state.location.pathname).toBe(expectedNewPath);
     });
   });
 });
