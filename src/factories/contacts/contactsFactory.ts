9552607ccb28b3762df0a55090da9cd18964c391
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-06-01 15:35:38 +0200

    Add contactFactory and contact/s  mocks

---
+++ src/factories/contacts/contactsFactory.ts
@@ -0,0 +1,26 @@
+import { Factory } from "fishery";
+import { faker } from "@faker-js/faker";
+import { ContactStructure } from "../../types";
+
+const contactFactory = Factory.define<ContactStructure>(() => ({
+  id: faker.database.mongodbObjectId.toString(),
+  name: faker.person.firstName(),
+  surname: faker.person.lastName(),
+  phoneNumber: {
+    mobile: faker.phone.number(),
+  },
+  address: faker.location.direction(),
+  avatar: "https://ui-avatars.com/api/?name=John+Doe",
+  birthday: faker.date.birthdate.toString(),
+  email: faker.internet.email(),
+  socials: {
+    instagram: "https://instagram.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+  },
+  user: "646f6a0da1b8a16b45eabf43",
+}));
+
+export const contactMock = contactFactory.build();
+
+export const contactsMock = contactFactory.buildList(3);
