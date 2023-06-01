12769fdc2fd270f36db9d8f7d8e128d42c8addf6
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-06-01 12:24:31 +0200

    Add msw errorHandlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,35 +1,26 @@
 import { rest } from "msw";
-import { userLoginDataMock, userTokenMock } from "./user/userMocks";
+import { userTokenMock } from "./user/userMocks";
 import { paths } from "../constants";
-import { UserCredentials } from "../types";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
-interface Request {
-  body: UserCredentials;
-}
-
 export const handlers = [
-  rest.post(`${apiUrl}${paths.userLogin}`, (req: Request, res, ctx) => {
-    const { body } = req;
-    const { username, password } = body;
-
-    if (
-      username !== userLoginDataMock.username ||
-      password !== userLoginDataMock.password
-    ) {
-      return res(
-        ctx.status(401),
-        ctx.json({
-          error: "Wrong credentials",
-        })
-      );
-    }
-
+  rest.post(`${apiUrl}${paths.userLogin}`, (_req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
         token: userTokenMock,
+      })
+    );
+  }),
+];
+
+export const errorHandlers = [
+  rest.post(`${apiUrl}${paths.userLogin}`, (_req, res, ctx) => {
+    return res(
+      ctx.status(401),
+      ctx.json({
+        error: "Wrong credentials",
       })
     );
   }),
