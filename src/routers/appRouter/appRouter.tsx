4ff912a87a0ab5ec72eaf7b288295d3ea3c4353a
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-06-01 12:52:41 +0200

    Add Contacts Page component

--- src/routers/appRouter/appRouter.tsx
+++ src/routers/appRouter/appRouter.tsx
@@ -2,6 +2,7 @@ import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../../components/App/App";
 import LoginPage from "../../pages/LoginPage/LoginPage";
 import { paths } from "../../constants";
+import ContactsPage from "../../pages/ContactsPage/ContactsPage";
 
 const routes: RouteObject[] = [
   {
@@ -18,7 +19,7 @@ const routes: RouteObject[] = [
       },
       {
         path: paths.contacts,
-        element: <p>Contacts page</p>,
+        element: <ContactsPage />,
       },
     ],
   },
