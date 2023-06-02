36da0e8927ec8d645259bf31e9faace3a76bffc6
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-06-02 17:22:51 +0200

    Add test for hideLoading

--- src/store/ui/uiReducer.test.ts
+++ src/store/ui/uiReducer.test.ts
@@ -1,16 +1,32 @@
 import { shownLoadingStateMock } from "../../mocks/ui/uiStateMocks";
 import { hiddenLoadingStateMock } from "../../mocks/ui/uiStateMocks";
 import { UiStateStructure } from "../../types";
-import { showLoadingActionCreator, uiReducer } from "./uiSlice";
+import {
+  hideLoadingActionCreator,
+  showLoadingActionCreator,
+  uiReducer,
+} from "./uiSlice";
 
 describe("Given a uiReducer", () => {
-  describe("When it receives an initial state and a showLoading action", () => {
-    test("Then it should return a new state with isLoading to true", () => {
+  describe("When it receives an current state and a showLoading action", () => {
+    test("Then it should return a new state with isLoading set to true", () => {
       const currentUiState: UiStateStructure = hiddenLoadingStateMock;
 
       const expectedUiState: UiStateStructure = shownLoadingStateMock;
 
       const newState = uiReducer(currentUiState, showLoadingActionCreator());
+
+      expect(newState).toStrictEqual(expectedUiState);
+    });
+  });
+
+  describe("When it receives an current state and a hideLoading action", () => {
+    test("Then it should return a new state with isLoading set to false", () => {
+      const currentUiState: UiStateStructure = shownLoadingStateMock;
+
+      const expectedUiState: UiStateStructure = hiddenLoadingStateMock;
+
+      const newState = uiReducer(currentUiState, hideLoadingActionCreator());
 
       expect(newState).toStrictEqual(expectedUiState);
     });
