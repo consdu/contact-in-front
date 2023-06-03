7296501e1e45cf7afef541be8fe9162a8aa2729e
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-06-03 13:06:01 +0200

    Add test for valid user loggin in and seeing contacts

--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -6,21 +6,39 @@ import { renderWithProviders, wrapWithRouter } from "../../testUtils/testUtils";
 import {
   emptyUserStateMock,
   loggedUserStateMock,
-  correctCredentialsMock,
 } from "../../mocks/user/userMocks";
 import { paths } from "../../constants";
 import { server } from "../../mocks/server";
 import { errorHandlers } from "../../mocks/handlers";
 import ContactsPage from "../ContactsPage/ContactsPage";
+import { contactsMock } from "../../factories/contacts/contactsFactory";
+import { UserCredentials } from "../../types";
+import { correctCredentialsMock } from "../../mocks/user/userMocks";
+import { incorrectCredentialsMock } from "../../mocks/user/userMocks";
 
 beforeEach(() => {
   localStorage.clear();
 });
 
-describe("Given a LoginPage component", () => {
+const loginUser = async (credentials: UserCredentials) => {
   const usernameLabel = "Username:";
   const passwordLabel = "Password:";
   const buttonText = "Login";
+
+  const usernameInput = screen.getByLabelText(usernameLabel);
+  const passwordInput = screen.getByLabelText(passwordLabel);
+  const loginbutton = screen.getByRole("button", {
+    name: buttonText,
+  });
+
+  await userEvent.type(usernameInput, credentials.username);
+  await userEvent.type(passwordInput, credentials.password);
+  await userEvent.click(loginbutton);
+};
+
+describe("Given a LoginPage component", () => {
+  const appName = "contact";
+  const appLogoText = "in";
 
   const routes = [
     {
@@ -35,9 +53,6 @@ describe("Given a LoginPage component", () => {
 
   describe("When rendered", () => {
     test("Then it should show the title and logo of the app", () => {
-      const appName = "contact";
-      const appLogoText = "in";
-
       renderWithProviders(wrapWithRouter(<LoginPage />));
 
       const appTitle = screen.getByText(appName);
@@ -54,39 +69,40 @@ describe("Given a LoginPage component", () => {
 
       renderWithProviders(<RouterProvider router={router} />);
 
-      const usernameInput = screen.getByLabelText(usernameLabel);
-      const passwordInput = screen.getByLabelText(passwordLabel);
-      const loginbutton = screen.getByRole("button", {
-        name: buttonText,
-      });
-
-      await userEvent.type(usernameInput, correctCredentialsMock.username);
-      await userEvent.type(passwordInput, correctCredentialsMock.password);
-      await userEvent.click(loginbutton);
+      await loginUser(correctCredentialsMock);
 
       expect(router.state.location.pathname).toBe(paths.contacts);
     });
-  });
 
-  describe("When rendered and the user logs in with invalid credentials", () => {
-    test("Then it should stay on the login page", async () => {
-      server.resetHandlers(...errorHandlers);
-
+    test("Then it should show the user's collection of contacts", async () => {
       const router = createMemoryRouter(routes);
 
       renderWithProviders(<RouterProvider router={router} />, {
         user: emptyUserStateMock,
       });
 
-      const usernameInput = screen.getByLabelText(usernameLabel);
-      const passwordInput = screen.getByLabelText(passwordLabel);
-      const loginbutton = screen.getByRole("button", {
-        name: buttonText,
+      await loginUser(correctCredentialsMock);
+
+      contactsMock.forEach((contact) => {
+        const contactName = screen.getByRole("heading", {
+          name: `${contact.name} ${contact.surname}`,
+        });
+
+        expect(contactName).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe("When rendered and the user logs in with invalid credentials", () => {
+    test("Then it should stay on the login page", async () => {
+      server.resetHandlers(...errorHandlers);
+      const router = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={router} />, {
+        user: emptyUserStateMock,
       });
 
-      await userEvent.type(usernameInput, "wrong username");
-      await userEvent.type(passwordInput, "wrong password");
-      await userEvent.click(loginbutton);
+      await loginUser(incorrectCredentialsMock);
 
       expect(router.state.location.pathname).toBe("/");
     });
