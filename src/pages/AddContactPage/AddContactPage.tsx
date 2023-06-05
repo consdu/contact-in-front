94efe6ca2e72cab5c7c0c8e55bd38841d520b949
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-06-05 14:45:27 +0200

    Add AddContactPage component and styles

---
+++ src/pages/AddContactPage/AddContactPage.tsx
@@ -0,0 +1,15 @@
+import AddContactPageStyled from "./AddContactPageStyled";
+
+const AddContactPage = () => {
+  return (
+    <AddContactPageStyled>
+      <header className="header">
+        <h2 className="header__title">
+          new <span>contact</span>
+        </h2>
+      </header>
+    </AddContactPageStyled>
+  );
+};
+
+export default AddContactPage;
