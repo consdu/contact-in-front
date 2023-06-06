e9682c9257814a0b68cf4ee01db1b26d0d2ea934
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-06-06 14:40:00 +0200

    Add test for ContactForm inputs typing in funcionality

--- src/components/ContactForm/ContactForm.test.tsx
+++ src/components/ContactForm/ContactForm.test.tsx
@@ -1,28 +1,28 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import ContactForm from "./ContactForm";
 import { renderWithProviders } from "../../testUtils/testUtils";
 
 describe("Given a Contact Form component", () => {
+  const buttonText = "create";
+  const inputLabels = [
+    "Name",
+    "Surname",
+    "Phone number",
+    "Email",
+    "Address",
+    "Date of birth",
+    "Image",
+    "Twitter",
+    "Instagram",
+    "Linkedin",
+  ];
+
   describe("When it receives 'create' as a text and it's rendered", () => {
-    const buttonText = "create";
-
     test("Then it should show Name, Surname, Phone, Email, Address, DOB, Image, Socials inputs", () => {
-      const inputNames = [
-        "Name",
-        "Surname",
-        "Phone number",
-        "Email",
-        "Address",
-        "Date of birth",
-        "Image",
-        "Twitter",
-        "Instagram",
-        "Linkedin",
-      ];
-
       renderWithProviders(<ContactForm buttonText={buttonText} />);
 
-      inputNames.forEach((inputName) => {
+      inputLabels.forEach((inputName) => {
         const input = screen.getByLabelText(inputName);
 
         expect(input).toBeInTheDocument();
@@ -37,6 +37,24 @@ describe("Given a Contact Form component", () => {
       });
 
       expect(submitButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When is rendered and the user types 'example' in the inputs", () => {
+    test("Then the inputs should show 'example'", () => {
+      const textSample = "example";
+
+      renderWithProviders(<ContactForm buttonText={buttonText} />);
+
+      inputLabels.forEach(async (inputName) => {
+        if (inputName !== "Date of birth") {
+          const input = screen.getByLabelText(inputName);
+
+          await userEvent.type(input, textSample);
+
+          expect(input).toHaveValue(textSample);
+        }
+      });
     });
   });
 });
