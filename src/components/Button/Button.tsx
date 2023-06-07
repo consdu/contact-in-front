ca6dd10ce636bd31a9a5acdb3547baa55bf3b4a0
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-06-07 23:17:52 +0200

    Add reusable Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,33 @@
+import ButtonStyled from "./ButtonStyled";
+
+interface ButtonProps {
+  isLoading: boolean;
+  isButtonDisabled: boolean;
+  type?: "button" | "submit" | "reset" | undefined;
+  text: string;
+}
+
+const Button = ({
+  isLoading,
+  isButtonDisabled,
+  text,
+  type,
+}: ButtonProps): React.ReactElement => {
+  let classes = "button ";
+
+  if (isLoading) {
+    classes += "button--loading button--hide-text";
+  }
+
+  return (
+    <ButtonStyled
+      className={classes}
+      disabled={isButtonDisabled || isLoading}
+      type={type}
+    >
+      {text}
+    </ButtonStyled>
+  );
+};
+
+export default Button;
---
+++ src/components/Button/ButtonStyled.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  position: relative;
+  width: 100%;
+  background-color: ${(props) => props.theme.colors.primary600};
+  color: #fff;
+  text-transform: uppercase;
+  font-size: 1.5rem;
+  padding-block: 20px;
+  border-radius: 5px;
+  margin-top: 30px;
+  box-shadow: 0px 3px 5px -1px ${(props) => props.theme.colors.gray500};
+  transition: background-color 0.2s ease;
+
+  &:disabled {
+    background-color: transparent;
+    border: 2px solid ${(props) => props.theme.colors.primary600};
+    color: ${(props) => props.theme.colors.primary600};
+  }
+
+  &:disabled.button--loading {
+    background-color: ${(props) => props.theme.colors.primary600};
+    border: none;
+  }
+
+  &.button--hide-text {
+    color: transparent;
+  }
+
+  &.button--loading::after {
+    content: "";
+    position: absolute;
+    width: 32px;
+    height: 32px;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+    border: 4px solid transparent;
+    border-top-color: #ffffff;
+    border-radius: 50%;
+    animation: button-loading-spinner 1s ease infinite;
+  }
+
+  @keyframes button-loading-spinner {
+    from {
+      transform: rotate(0turn);
+    }
+
+    to {
+      transform: rotate(1turn);
+    }
+  }
+`;
+
+export default ButtonStyled;
