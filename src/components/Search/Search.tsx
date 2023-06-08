582d0da3e34a8d3d98cb06cd9620dcd1280fe175
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-06-08 12:17:27 +0200

    Add Search component and styles

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,22 @@
+import { IoSearch } from "react-icons/io5";
+import SearchStyled from "./SearchStyled";
+
+const Search = () => {
+  return (
+    <SearchStyled className="search-group">
+      <label className="search-group__label" htmlFor="search">
+        <span className="search-group__icon">
+          <IoSearch />
+        </span>
+        <input
+          className="search-group__control"
+          type="text"
+          id="search"
+          placeholder="Search"
+        />
+      </label>
+    </SearchStyled>
+  );
+};
+
+export default Search;
---
+++ src/components/Search/SearchStyled.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const SearchStyled = styled.section`
+  font-size: 1.2rem;
+
+  .search-group {
+    &__label {
+      display: flex;
+      background-color: ${(props) => props.theme.colors.gray200};
+      border-radius: ${(props) => props.theme.radius.normal};
+    }
+
+    &__control {
+      width: 100%;
+      padding: 10px;
+      background-color: ${(props) => props.theme.colors.gray200};
+      border-radius: ${(props) => props.theme.radius.normal};
+      outline: none;
+    }
+
+    &__icon {
+      font-size: 1.5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 15px;
+      padding-right: 5px;
+      color: ${(props) => props.theme.colors.gray800};
+    }
+  }
+`;
+
+export default SearchStyled;
