e2a10b3fc515b5864724926ab30d54209a089eb7
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-11-06 10:48:15 +0100

    Reander loading spinner only if request takes more than 200ms

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,10 +1,25 @@
+import React, { useState, useEffect } from "react";
 import LoadingStyled from "./LoadingStyled";
 
 const Loading = (): React.ReactElement => {
+  const [showLoading, setShowLoading] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLoading(true);
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <LoadingStyled>
-      <div className="loader" aria-label="loading animation"></div>
-    </LoadingStyled>
+    <>
+      {showLoading && (
+        <LoadingStyled>
+          <div className="loader" aria-label="loading animation"></div>
+        </LoadingStyled>
+      )}
+    </>
   );
 };
 
