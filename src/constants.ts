b1533598b8e2a401b64a1504069fbac80871a951
Constantin Dusescu <constantin.dusescu@icloud.com>
2023-11-06 10:49:46 +0100

    Fix selectedContact initial value load value to be empty object

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
-import { ContactStructure } from "./types";
-
 export const paths = {
   userLogin: "/user/login",
   login: "/login",
@@ -31,24 +29,4 @@ export const responseErrors = {
 export const responseMesssages = {
   deleteSuccessful: "Contact deleted succesfully",
   updateSuccessful: "Contact updated succesfully",
-};
-
-export const emptySelectedContact: ContactStructure = {
-  id: "",
-  user: "",
-  address: "",
-  avatar:
-    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODAiIGhlaWdodD0iODAiIHZpZXdCb3g9IjAgMCA4MCA4MCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPGNpcmNsZSBvcGFjaXR5PSIwLjIiIGN4PSI0MCIgY3k9IjQwIiByPSI0MCIgZmlsbD0iI0YxRjhGNCIvPgo8L3N2Zz4K",
-  birthday: "01-01-2000",
-  email: "",
-  name: "﹒",
-  surname: "",
-  phoneNumber: {
-    mobile: "",
-  },
-  socials: {
-    twitter: "",
-    instagram: "",
-    linkedin: "",
-  },
 };
